e91790e2c6d467279f79e825d3f0af5e011926c0
Eviper01 <eviper01@gmail.com>
2021-11-01 20:25:27 +0800

    Large improvements to the SASM language

--- ToolChain/SASM_X8-64k.py
+++ ToolChain/SASM_X8-64k.py
@@ -38,30 +38,92 @@ translator = {"NOP": "10111111",
 
 literal_translator = {"STC": "0"}
 
-try:
-    f = open(sys.argv[1], 'r')
+def read_sasm(file):
+    f = open(file, 'r')
     program = f.readlines()
     f.close()
-except:
-    print("No Input file was specified")
-    exit()
-assembled = []
-try:
-    for i in program:
-        if(i.split()[0].upper() in literal_translator):
-            bytestring = '{0:07b}'.format(int(i.split()[1]))
-            assembled.append(literal_translator[i[0:3].upper()] + bytestring)
-        else:
-            assembled.append(translator[i.split()[0].upper()])
-except:
-    print("Assembly Failed, Unkown OPCode:")
-    print(i)
+    return program
+
+def intepret_constants(program):
+    labels = []
+    pointers = []
+    for i in range(len(program)):
+        if program[i].split()[0][0] == "&":
+            # print("".join(list(program[i].split()[0][1:len(program[i].split()[0])])))
+            label = program[i].split()[0]
+            if label not in labels:
+                labels.append(label)
+                pointers.append(i)
+            else:
+                print("warning: line ({line}) pointer {pointer} already used @ line ({line2})".format(line=i, pointer=label, line2=pointers[labels.index(label)]))
+            line = program[i].split()
+            line.pop(0)
+            program[i] = " ".join(line)
+
+    for i in range(len(pointers)):
+        for j in range(len(program)):
+            program[j] = program[j].replace(labels[i], str(pointers[i]))
+    return program
+
+def intepret_opcodes(program):
+    assembled = []
+    try:
+        for i in program:
+            if(i.split()[0].upper() in literal_translator):
+                bytestring = '{0:07b}'.format(int(i.split()[1]))
+                assembled.append(literal_translator[i[0:3].upper()] + bytestring)
+            else:
+                assembled.append(translator[i.split()[0].upper()])
+    except:
+        print("Assembly Failed, Unkown OPCode:")
+        print(i)
+        exit()
+    return assembled
+
+def hex_format(item):
+    hexer = str(hex(int(item, 2)))
+    hexer = hexer[2:len(str(hex(int(item, 2))))]
+    if len(hexer) == 1:
+        return "0" + hexer
+    else:
+        return hexer
+
+
+def write_hex(assembled, filename):
+    with open(filename, 'w') as f:
+        for item in assembled:
+            f.write("%s" % hex_format(item))
+        f.write("\n")
+    print("compiled to out.hex as hex")
+    return 0
+
+def write_binary(assembled, filename):
+    with open(filename, 'w') as f:
+        for item in assembled:
+            f.write("%s\n" % item)
+    print("Compiled to out.bin as binary")
+    return 0
+
+def main():
+
+
+    try:
+        program = read_sasm(sys.argv[1])
+    except:
+        print("Wrong Input file  specified")
+        exit()
+
+
+
+    program = intepret_constants(program)
+    assembled = intepret_opcodes(program)
+    try:
+        if sys.argv[2] == "-h":
+            write_hex(assembled, "out.hex")
+            exit()
+    except Exception as e:
+        pass
+    write_binary(assembled, "out.bin")
     exit()
 
-with open('out.bin', 'w') as f:
-    for item in assembled:
-        f.write("%s\n" % item)
-print("Compiled to out.bin")
-#
-
-#assembler upload sequence
+main()
